94adada9cb09cea45729bc443749676b17b72535
Bruno Desprez <bruno.desprez@gmail.com>
2016-03-20 00:46:44 +0100

    [-] Les services ne doivent pas contenir de camel case

---
+++ services/Err404.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace covoiturage\pages;
+
+// Vue
+use covoiturage\classes\abstraites\ServiceVue;
+
+/**
+ * Description of 404
+ *
+ * @author bruno
+ */
+class Err404 extends ServiceVue {
+
+    /**
+     * Page 404
+     */
+    public function executerService() {
+        echo '<div id="404" class="bg-danger text-center"><h3>Page inconnue !</h3></div>';
+    }
+
+    /**
+     * Titre de la page
+     * @return string
+     */
+    public function getTitre() {
+        return 'Erreur 404';
+    }
+
+    /**
+     * La page n'est pas sécurisé
+     * @return boolean
+     */
+    public function isSecurised() {
+        return FALSE;
+    }
+}
